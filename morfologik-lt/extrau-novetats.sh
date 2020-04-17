64799410870416a89db784ac73329b880721f7ac
jaumeortola <jaumeortola@gmail.com>
2020-04-17 20:39:47 +0200

    érase

--- morfologik-lt/extrau-novetats.sh
+++ morfologik-lt/extrau-novetats.sh
@@ -20,13 +20,11 @@ echo "Ordenant diccionari"
 export LC_ALL=C && sort -u diccionari_antic.txt -o diccionari_antic.txt
 echo "Comparant diccionaris"
 diff ../resultats/lt/diccionari.txt diccionari_antic.txt > diff.txt
-cp diff.txt novetats_amb_tag.txt
+
 echo "Extraient novetats"
-sed -i '/ aguar /d' novetats_amb_tag.txt #excloure aguar
-sed -i 's/^[^<].*$//g' novetats_amb_tag.txt
+grep -E "^< " diff.txt > novetats_amb_tag.txt
 sed -i 's/^< //g' novetats_amb_tag.txt
 sed -i 's/ /\t/g' novetats_amb_tag.txt
-sed -i '/^\s*$/d' novetats_amb_tag.txt
 cp novetats_amb_tag.txt novetats_sense_tag.txt
 sed -i 's/^\(.*\)\t\(.*\)\t\(.*\)$/\1/' novetats_sense_tag.txt
 sed -i '/^\s*$/d' novetats_sense_tag.txt
@@ -35,5 +33,26 @@ cat spelling.head novetats_sense_tag.txt > spelling.txt
 cat manual-tagger.head novetats_amb_tag.txt > manual-tagger.txt
 cp manual-tagger.txt /home/jaume/github/languagetool/languagetool-language-modules/es/src/main/resources/org/languagetool/resource/es/
 cp spelling.txt /home/jaume/github/languagetool/languagetool-language-modules/es/src/main/resources/org/languagetool/resource/es/
-echo "Resultats en spelling.txt manual-tagger.txt"
-# emacs novetats_* &
+
+echo "Extraient paraules esborrades"
+grep -E "^> " diff.txt > removed.txt
+sed -i 's/^> //g' removed.txt
+sed -i 's/ /\t/g' removed.txt
+sed -i '/^\s*$/d' removed.txt
+#cp novetats_amb_tag.txt novetats_sense_tag.txt
+#sed -i 's/^\(.*\)\t\(.*\)\t\(.*\)$/\1/' novetats_sense_tag.txt
+#sed -i '/^\s*$/d' novetats_sense_tag.txt
+#export LC_ALL=C && sort -u novetats_sense_tag.txt -o novetats_sense_tag.txt
+#cat spelling.head novetats_sense_tag.txt > spelling.txt
+cat removed-tagger.head removed.txt > removed-tagger.txt
+cp removed-tagger.txt /home/jaume/github/languagetool/languagetool-language-modules/es/src/main/resources/org/languagetool/resource/es/
+#cp spelling.txt /home/jaume/github/languagetool/languagetool-language-modules/es/src/main/resources/org/languagetool/resource/es/
+
+echo "Resultats en spelling.txt manual-tagger.txt removed-tagger.txt"
+
+rm diff.txt
+rm manual-tagger.txt
+rm removed-tagger.txt
+rm removed.txt
+rm spelling.txt
+rm novetats*
