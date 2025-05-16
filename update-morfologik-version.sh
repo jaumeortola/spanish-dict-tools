ca63a127d8394e9356e3bfe8e1da5aeb20051d94
jaumeortola <jaumeortola@gmail.com>
2025-05-16 13:20:06 +0200

    update dictVers

--- update-morfologik-version.sh
+++ update-morfologik-version.sh
@@ -1,4 +1,4 @@
-dictVers=2.3
+dictVers=2.4
 unzip ~/.m2/repository/org/softcatala/spanish-pos-dict/${dictVers}/spanish-pos-dict-${dictVers}.jar
 cp org/languagetool/resource/es/* morfologik-lt/
 rm -rf org/
